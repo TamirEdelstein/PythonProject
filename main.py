67fb309afd953932bf6ef94ed529c40795a1c8f8
TamirEdelstein <tamir4989@gmail.com>
2026-01-22 11:51:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import streamlit as st
 
 st.title('Weather App')
 
+st.write("Welcome to the Israel Public Transit Analytics App 📊 !")
+
+
 name = st.text_input('Enter your name:', '')
 if name:
     st.write(f'Hello {name}!,welcome to Weather App!')
+
+
