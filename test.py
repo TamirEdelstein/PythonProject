9f714e67b08f4e728441f630f9cbb5aab4e9470c
TamirEdelstein <tamir4989@gmail.com>
2026-01-22 12:03:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ import numpy as np
 st.write("Welcome to the Israel Public Transit Analytics App 📊 !")
 
 
+name = st.text_input('Enter your name:', '')
+if name:
+    st.write(f'Hello {name}!,welcome to Weather App!')
+
+
