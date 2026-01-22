ecda4f0ba65733a0cedcafe510681321e27ec21c
TamirEdelstein <tamir4989@gmail.com>
2026-01-22 12:14:49 +0200

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 import numpy as np
+import plotly.express as px
 
 st.write("Welcome to the Israel Public Transit Analytics App 📊 !")
 
@@ -25,4 +26,12 @@ rides['day_of_week'] = rides['scheduled_start_time'].dt.day_name()
 rides = rides[['id', 'siri_route_id', 'scheduled_start_time', 'duration_minutes', 'hour', 'day_of_week']]
 
 st.dataframe(rides)
-st.line_chart(rides, x='hour', y='duration_minutes')
+
+# Create a dropdown in the sidebar
+selected_day = st.sidebar.selectbox('Select a Day', rides['day_of_week'].unique())
+# Filter data based on selection
+filtered_rides = rides[rides['day_of_week'] == selected_day]
+# Plot the dynamic data
+fig = px.line(filtered_rides, x='hour', y='duration_minutes',
+              title=f'Ride Duration by Hour on {selected_day}')
+st.plotly_chart(fig)
